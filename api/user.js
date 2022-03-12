dd685728d37c0d80c8458f40301dffe5ad0981ab
jivko25 <jivkou@yahoo.com>
2022-03-12 20:57:34 +0200

    test fix

--- api/user.js
+++ api/user.js
@@ -8,11 +8,11 @@ const secret = 'ndaisndiasnduinas9j32984u910sandjiasn';
 router.post('/login', async (req, res) => {
     //Validate body
     const error = loginValidate(req.body);
-    if(error) return res.status(400).send(error);
+    if(error) return res.status(400).json(error);
 
     //Check if email exist
     const userToLogin = await User.findOne({email : req.body.email});
-    if(!userToLogin) return res.status(400).send('Email does not exist!');
+    if(!userToLogin) return res.status(400).json('Email does not exist!');
 
     //Check if password match
     // const isValidPassword = await bcrypt.compare(req.body.password, userToLogin.password);
@@ -20,7 +20,7 @@ router.post('/login', async (req, res) => {
     // if(!isValidPassword) return res.status(400).send('Invalid email or password')
 
     const token = jwt.sign({_id: userToLogin._id}, secret);
-    res.header('token', token).send(token);
+    res.header('token', token).json(token);
 })
 
 module.exports = router;
