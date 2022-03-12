0d988b426e5c99e12cdd53292b393a97fdc97881
jivko25 <jivkou@yahoo.com>
2022-03-12 20:42:26 +0200

    add connection to mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const product = require("./api/product");
+const mongoose = require('mongoose');
 
 app.use(express.json({ extended: false }));
+
+mongoose.connect('mongodb+srv://jivko25:jivko25@cluster0.detoa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', () => console.log('connected to DB'));
 
 
 app.use("/api/product", product);
