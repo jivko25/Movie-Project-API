8dd9af9a91e3cf53565d4109f36a12a137ce9c49
jivko25 <jivkou@yahoo.com>
2022-03-13 17:54:28 +0200

    remove product route

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const product = require("./api/product");
 const user = require('./api/user');
 const movie = require('./api/movie');
 const mongoose = require('mongoose');
@@ -10,7 +9,6 @@ app.use(express.json({ extended: false }));
 mongoose.connect('mongodb+srv://jivko25:jivko25@cluster0.detoa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', () => console.log('connected to DB'));
 
 
-app.use("/api/product", product);
 app.use('/api/user', user);
 app.use('/api/movie', movie);
 
